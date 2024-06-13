be2c31361aeb5a3c1557ca55501ba9b7a7ae1c01
chung-v <vcdott@gmail.com>
2024-06-13 20:30:40 +1000

    Sets examples

--- sets.py
+++ sets.py
@@ -14,3 +14,23 @@ print(my_set)
 # Membership testing
 print(2 in my_set)
 print(3 in my_set)
+
+# Set operations
+set_a = {1, 2, 3}
+set_b = {3, 4, 5}
+
+# Union
+union_value = set_a.union(set_b)
+print(union_value)
+
+# Intersection
+intersection_value = set_a.intersection(set_b)
+print(intersection_value)
+
+# Difference - set_a values minus intersection value
+difference_value = set_a.difference(set_b)
+print(difference_value)
+# Difference - set_b values minus intersection value
+difference_value = set_b.difference(set_a)
+print(difference_value)
+
